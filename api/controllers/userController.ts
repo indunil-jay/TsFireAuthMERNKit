c733eeadc0cc01f36a608738c29afe36d084a3b9
indunil-jay <indunil.jayz@gmail.com>
2024-03-24 23:51:51 +0530

    create functionality for saving data in db and password encryption and also create custome error class

--- api/controllers/userController.ts
+++ api/controllers/userController.ts
@@ -1,6 +1,43 @@
 import { Request, Response, NextFunction } from "express";
+import User, { IUser, IUserDocument } from "../models/userModel";
+import AppError from "../utils/appError";
 
-export const signup = (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.body);
-  res.status(200).json({ status: "success", message: "end point is working" });
+export const signup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, password, passwordConfirm }: IUser = req.body;
+
+    //1) check both passwords are same
+    if (password !== passwordConfirm) {
+      return next(new AppError("passwords are not macthing.", 400));
+    }
+
+    //2) before saving database we need to encrpt password.(use middleware)
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+      passwordConfirm,
+    });
+
+    //3) remove password from output when sending response
+    const { password: userPassword, ...userData } = user.toObject();
+
+    res.status(200).json({ status: "success", data: { user: userData } });
+  } catch (error) {
+    // If an error occurs, send error response
+    if (error instanceof AppError) {
+      // Handle custom error with statusCode
+      res
+        .status(error.statusCode)
+        .json({ status: "fail", message: error.message });
+    } else {
+      // Handle other errors
+      res.status(500).json({ status: "error", message: error });
+    }
+  }
 };
--- api/models/userModel.ts
+++ api/models/userModel.ts
@@ -1,22 +1,25 @@
 import mongoose, { Document, Model } from "mongoose";
+import validator from "validator";
+import bcryptjs from "bcryptjs";
 
 //define user shape
-export interface User {
+export interface IUser {
   name: string;
   email: string;
   password: string;
+  passwordConfirm: string | undefined;
 }
 
 //define user document shape
-export interface UserDocument extends Document, User {
+export interface IUserDocument extends Document, IUser {
   createdAt: Date;
   updatedAt: Date;
 }
 
 //define user model shape
-interface UserModel extends Model<UserDocument> {}
+export interface IUserModel extends Model<IUserDocument> {}
 
-const userSchema = new mongoose.Schema<UserDocument, UserModel>(
+const userSchema = new mongoose.Schema<IUserDocument, IUserModel>(
   {
     name: {
       type: String,
@@ -27,17 +30,44 @@ const userSchema = new mongoose.Schema<UserDocument, UserModel>(
       required: [true, "Please provide your email."],
       unique: true,
       lowercase: true,
+      validate: [validator.isEmail, "Please Provide a valid password"],
     },
     password: {
       type: String,
       required: [true, "Please provide a password."],
       minlength: 8,
+      select: false,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, "Please provide a password."],
+      minlength: 8,
+      // validate: {
+      //   validator: function (this: IUser, el: string) {
+      //     //this validation method only work .onSave() and .create()
+      //     return el === this.password;
+      //   },
+      //   message: "passwords are not matching. Please enter matching password.",
+      // },
     },
   },
   { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
-const User: Model<UserDocument> = mongoose.model<UserDocument, UserModel>(
+userSchema.pre("save", async function (this: IUserDocument, next) {
+  //if password is modified then return;
+  if (!this.isModified("password")) return next();
+
+  //encryptpassword when create new user
+  this.password = await bcryptjs.hash(this.password, 12);
+
+  //delete confirm  password, it is not necessary for save
+  this.passwordConfirm = undefined;
+
+  next();
+});
+
+const User: Model<IUserDocument> = mongoose.model<IUserDocument, IUserModel>(
   "User",
   userSchema
 );
