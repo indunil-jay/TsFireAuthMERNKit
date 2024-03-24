99a0b260c9f0f69ba4dbabe39181bd43266e2875
indunil-jay <indunil.jayz@gmail.com>
2024-03-25 04:10:07 +0530

    created unhandle promise rejection handling functionality

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,4 +7,15 @@ dotenv.configDotenv({ path: "./.env" });
 connectToMongoDB(process.env.MONGODB_CONNECTION as string);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`app running on port ${port}...`));
+const server = app.listen(port, () =>
+  console.log(`app running on port ${port}...`)
+);
+
+//unhandle promise rejection handling (if somewhere in application exits unhadle promise rejection it will catch)
+process.on("unhandledRejection", (error: Error) => {
+  console.log(error.name, error.message);
+  console.log("unhandledRejection!! app is shutting down...");
+  server.close(() => {
+    process.exit(1);
+  });
+});
