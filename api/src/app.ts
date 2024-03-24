5de1981ff76e1c6c7dcd2291e92e14d211c05446
indunil-jay <indunil.jayz@gmail.com>
2024-03-25 01:20:11 +0530

    add functionality for handle unhandle routes request error

--- api/src/app.ts
+++ api/src/app.ts
@@ -8,12 +8,19 @@ const app = express();
 if ((process.env.NODE_ENV as string) === "development") {
   app.use(morgon("dev"));
 }
-console.log(process.env.NODE_ENV);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(cors());
 
 app.use("/api/v1/users", userRouter);
+
+// Handle unhandle routes  request error
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!.`,
+  });
+});
 
 export default app;
