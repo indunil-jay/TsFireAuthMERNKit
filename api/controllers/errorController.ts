27fe2a97efc334aa8c8a755f36b268dc11571886
indunil-jay <indunil.jayz@gmail.com>
2024-03-25 02:58:36 +0530

    improve and refactor  global error handling middleware

--- api/controllers/errorController.ts
+++ api/controllers/errorController.ts
@@ -1,6 +1,31 @@
 import { NextFunction, Request, Response } from "express";
 import AppError from "../utils/appError";
 
+const sendErrorDev = (err: AppError, res: Response) => {
+  //Operational , trusted errors: Send message to client
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+      stack: err.stack,
+      error: err,
+    });
+  } else {
+    //Programming error or other unknown errors (better not leak details to the client.)
+    console.error("Error 🚨", err);
+    res
+      .status(500)
+      .json({ status: "error", message: "Something went very wrong!." });
+  }
+};
+
+const sendErrorProduction = (err: AppError, res: Response) => {
+  return res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
+
 const globalErrorHandler = (
   err: AppError,
   req: Request,
@@ -10,10 +35,13 @@ const globalErrorHandler = (
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  }
+
+  if (process.env.NODE_ENV === "production") {
+    sendErrorProduction(err, res);
+  }
 };
 
 export default globalErrorHandler;
